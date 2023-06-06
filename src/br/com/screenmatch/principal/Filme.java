e9904f4075fea5e9169bcc02b86c803e3756d04b
jeanheberth <jeanheberth19@gmail.com>
2023-06-06 14:05:43 -0300

    Initial commit

---
+++ src/br/com/screenmatch/principal/Filme.java
@@ -0,0 +1,12 @@
+package br.com.screenmatch;
+
+public class Filme {
+
+    String nome;
+    int anoDoLancamento;
+    boolean incluidoDoPlano;
+    double avaliacao;
+    int totalAvaliacoes;
+    int duracaoEmMinutos;
+
+}
