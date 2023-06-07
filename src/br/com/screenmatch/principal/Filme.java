703d144864bfc9ddc72dee037cfe41a9b9ff473a
jeanheberth <jeanheberth19@gmail.com>
2023-06-07 12:36:18 -0300

    Criando a regra do negócio.

--- src/br/com/screenmatch/principal/Filme.java
+++ src/br/com/screenmatch/principal/Filme.java
@@ -1,12 +1,44 @@
 package br.com.screenmatch.principal;
 
+import java.util.Calendar;
+
 public class Filme {
+    Calendar hoje = Calendar.getInstance();
 
     String nome;
     int anoDoLancamento;
-    boolean incluidoDoPlano;
-    double avaliacao;
+    double somaDasAvaliacoes;
     int totalAvaliacoes;
     int duracaoEmMinutos;
+    int idade;
 
+
+    void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lancamento do filme: " + anoDoLancamento);
+        System.out.println("Duracao do filme: " + duracaoEmMinutos + " minutos");
+    }
+
+    void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalAvaliacoes++;
+    }
+
+    double pegaMedia() {
+        return somaDasAvaliacoes / totalAvaliacoes;
+    }
+
+    int calculaIdade() {
+        return idade = (hoje.get(Calendar.YEAR) - (anoDoLancamento));
+    }
+
+    String descobrePlano() {
+        if (idade > 5) {
+            return "Nao incluso";
+        }
+        return "Incluso";
+    }
 }
+// Se a idade do filme for maior que 5 anos, ele nao está incluso no plano
+// Caso contrário está incluso.
+
--- src/br/com/screenmatch/principal/Principal.java
+++ src/br/com/screenmatch/principal/Principal.java
@@ -1,10 +1,32 @@
 package br.com.screenmatch.principal;
 
-public class Principal {
-    public static void main(String[] args) {
-        Filme meuFilme = new Filme();
-        meuFilme.nome = "Top gun Maverick";
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+public class Principal {
+
+
+    public static void main(String[] args) {
+        DecimalFormat decimalFormat = new DecimalFormat("#0.00");
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/YYYY");
+
+        Filme filme = new Filme();
+
+        filme.nome = "Top gun Maverick";
+        filme.anoDoLancamento = 1970;
+        filme.duracaoEmMinutos = 180;
+
+        filme.exibeFichaTecnica();
+        filme.avalia(9);
+        filme.avalia(7);
+        filme.avalia(9);
+
+        System.out.println("A soma das avaliacoes foi: " +filme.somaDasAvaliacoes);
+        System.out.println("O total de avaliacoes foi: " +filme.totalAvaliacoes);
+        System.out.println("O total de avaliacoes foi: " +decimalFormat.format(filme.pegaMedia()));
+        System.out.println("O Filme está incluso no plano? " +filme.descobrePlano());
+        System.out.println("A idade do filme é: " +filme.calculaIdade() + " anos.");
 
     }
 }
